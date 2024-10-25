f809dd5e149a046c887abbb889ff5167415dfcbd
Daniel wame <wamedaniel9@gmail.com>
2024-10-25 02:49:31 +0200

    disabling eslint in type declaration file

--- src/app/types/declarations.d.ts
+++ src/app/types/declarations.d.ts
@@ -1,6 +1,5 @@
-/* eslint-disable */
-
 declare module '@deriv/deriv-api/dist/DerivAPIBasic' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const DerivAPIBasic: any;
     export default DerivAPIBasic;
   }

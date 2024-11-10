2ad1f74db2d5da60293d118517a95f633abe0f40
Daniel wame <wamedaniel9@gmail.com>
2024-11-10 03:24:42 +0200

    edding tocken saving logic to local storage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 import { useEffect, useState } from 'react';
-import "./dash.css";
+import './dash.css';
 import Header from '../dashboard-header/page';
+import { useRouter } from 'next/navigation';
 
 interface UserAccount {
   account: string;
@@ -39,32 +40,36 @@ const Dashboard = () => {
   const [searchParams, setSearchParams] = useState<URLSearchParams | null>(null);
   const [authorizeData, setAuthorizeData] = useState<AuthorizeResponse | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
 
   // Initialize the search parameters when the component mounts
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     setSearchParams(params);
-  }, []);  // This effect runs only once when the component mounts
+  }, []); 
 
   // Get the query parameters after the searchParams state is set
   const acct1 = searchParams?.get('acct1');
   const token1 = searchParams?.get('token1');
   const cur1 = searchParams?.get('cur1');
-  const acct2 = searchParams?.get('acct2');
-  const token2 = searchParams?.get('token2');
-  const cur2 = searchParams?.get('cur2');
 
-  // Effect to handle the authorization request when query parameters are available
   useEffect(() => {
-    //   if (acct1 && token1 && cur1 && acct2 && token2 && cur2) {
-    if (acct1 && token1 && cur1 ) {
+    if (acct1 && token1 && cur1) {
       const accounts: UserAccount[] = [
         { account: acct1, token: token1, currency: cur1 },
-        // { account: acct2, token: token2, currency: cur2 },
       ];
       authorizeUser(accounts[0].token);
+    } else {
+      // Check if the token exists in local storage
+      const token = localStorage.getItem('userToken');
+      if (token) {
+        authorizeUser(token);
+      } else {
+        alert("No tocken found please login first1");
+        router.push('/');
+      }
     }
-  }, [acct1, token1, cur1, acct2, token2, cur2]); // Trigger effect only when query parameters change
+  }, [acct1, token1, cur1]); 
 
   const authorizeUser = async (token: string) => {
     console.log("Sending token:", token);
@@ -81,15 +86,20 @@ const Dashboard = () => {
       if (!response.ok) {
         const errorData = await response.json();
         console.error('Authorization failed:', errorData);
+        alert(errorData);
         throw new Error(`Authorization failed with status ${response.status}`);
       }
 
       const data = await response.json();
       console.log('Authorization successful:', data);
+
+      // Save the token and related data to localStorage
+      localStorage.setItem('userToken', token);
       setAuthorizeData(data);
     } catch (error) {
       setError('Error during authorization');
       console.error('Authorization error:', error);
+      alert(error);
     }
   };
 
@@ -97,56 +107,58 @@ const Dashboard = () => {
     <div className='w-full min-h-screen dashboard flex justify-center'>
       {error && <p style={{ color: 'red' }}>{error}</p>}
 
-      {authorizeData ? (
-        <div className='w-full h-fit flex flex-col justify-center insidediv'>
-          <div className='w-full dashboard-imge'>
-            <Header />
-            <div className='w-ful flex justify-center items-center personalinfo-div'>
-              <div className='namesandemail'>
-                <h1 className='fulname'>{authorizeData.authorize.authorize.fullname}</h1>
-                <p className='text-m emailinheader opacity-90'>{authorizeData.authorize.authorize.email}</p>
-              </div>
-              <div className='w-fit h-fit flex top-links'>
-                <button className="links-in-top flex flex row justify-items-center items-center inside-utton">
-                  Start Trading &gt;
-                </button>
-                <a className="links-in-top flex flex row justify-items-center items-center inside-utton2" href='/#how-it-work'>
-                  Current Analysis &gt;
-                </a>
+      {(
+        // If authorization data is present, display dashboard
+        authorizeData ? (
+          <div className='w-full h-fit flex flex-col justify-center insidediv'>
+            <div className='w-full dashboard-imge'>
+              <Header />
+              <div className='w-ful flex justify-center items-center personalinfo-div'>
+                <div className='namesandemail'>
+                  <h1 className='fulname'>{authorizeData.authorize.authorize.fullname}</h1>
+                  <p className='text-m emailinheader opacity-90'>{authorizeData.authorize.authorize.email}</p>
+                </div>
+                <div className='w-fit h-fit flex top-links'>
+                  <button className="links-in-top flex flex row justify-items-center items-center inside-utton">
+                    Start Trading &gt;
+                  </button>
+                  <a className="links-in-top flex flex row justify-items-center items-center inside-utton2" href='/#how-it-work'>
+                    Current Analysis &gt;
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className='mt-4 balancediv'>
-            <h2>My Balance</h2>
-            <h1>{authorizeData.authorize.authorize.balance} {authorizeData.authorize.authorize.currency}</h1>
-            <div className='w-ful flex justify-center mt-4 pt-2 small-info'>
-              <p className='text-m text-black opacity-70'>
-                <strong>Account Type:</strong> {authorizeData.authorize.authorize.account_list[0].account_type}
-              </p>
-              <p className='text-m text-black opacity-70'><strong>Login ID:</strong> {authorizeData.authorize.authorize.loginid}</p>
-              <p className='text-m text-black opacity-70'><strong>Country:</strong> {authorizeData.authorize.authorize.country}</p>
-              <p className='text-m text-black opacity-70'><strong>Local Currencies:</strong> {Object.keys(authorizeData.authorize.authorize.local_currencies).join(', ')}</p>
-              <p className='text-m text-black opacity-70'><strong>User ID:</strong> {authorizeData.authorize.authorize.user_id}</p>
-              <p className='text-m text-red-500 opacity-70'><strong className='text-red-500'>Broker:</strong> {authorizeData.authorize.authorize.landing_company_fullname}</p>
-              <p className='text-m text-black opacity-70'>{authorizeData.authorize.authorize.account_list[0].account_category} account</p>
+            <div className='mt-4 balancediv'>
+              <h2>My Balance</h2>
+              <h1>{authorizeData.authorize.authorize.balance} {authorizeData.authorize.authorize.currency}</h1>
+              <div className='w-ful flex justify-center mt-4 pt-2 small-info'>
+                <p className='text-m text-black opacity-70'>
+                  <strong>Account Type:</strong> {authorizeData.authorize.authorize.account_list[0].account_type}
+                </p>
+                <p className='text-m text-black opacity-70'><strong>Login ID:</strong> {authorizeData.authorize.authorize.loginid}</p>
+                <p className='text-m text-black opacity-70'><strong>Country:</strong> {authorizeData.authorize.authorize.country}</p>
+                <p className='text-m text-black opacity-70'><strong>Local Currencies:</strong> {Object.keys(authorizeData.authorize.authorize.local_currencies).join(', ')}</p>
+                <p className='text-m text-black opacity-70'><strong>User ID:</strong> {authorizeData.authorize.authorize.user_id}</p>
+                <p className='text-m text-red-500 opacity-70'><strong className='text-red-500'>Broker:</strong> {authorizeData.authorize.authorize.landing_company_fullname}</p>
+                <p className='text-m text-black opacity-70'>{authorizeData.authorize.authorize.account_list[0].account_category} account</p>
+              </div>
+            </div>
+            <div className='mt-8 balancediv'>
+              <h2>Current Analysis</h2>
+            </div>
+
+            <div className='mt-8 balancediv'>
+              <h2>Trade History</h2>
+            </div>
+
+            <div className='mt-8 balancediv'>
+              <h2>Profit and Loss</h2>
             </div>
           </div>
-
-          <div className='mt-8 balancediv'>
-            <h2>Current Analysis</h2>
-          </div>
-
-          <div className='mt-8 balancediv'>
-            <h2>Trade History</h2>
-          </div>
-
-          <div className='mt-8 balancediv'>
-            <h2>Profit and Loss</h2>
-          </div>
-        </div>
-      ) : (
-        <p className='mt-40'>Loading authorization...</p>
+        ) : (
+          <p className='mt-40'>Loading authorization...</p>
+        )
       )}
     </div>
   );
--- src/app/topSection/page.tsx
+++ src/app/topSection/page.tsx
@@ -1,23 +1,24 @@
 'use client';
 import './top.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from '../login/page';
 import anime from 'animejs';
-import { useEffect } from 'react'
+import { useRouter } from 'next/navigation';
 
 export default function TopSection() {
     const [showChatbot, setShowChatbot] = useState(false);
-    
+    const router = useRouter();
+
     useEffect(() => {
         // Anime.js for dir-normal element with opacity change
         anime({
             targets: '.dir-normal',
             translateX: 500,
-            opacity: [1,0.3], 
+            opacity: [1, 0.3], 
             direction: 'alternate',
             easing: 'easeInOutSine',
         });
-    
+
         // Anime.js for dir-reverse element with opacity change
         anime({
             targets: '.dir-reverse',
@@ -26,58 +27,70 @@ export default function TopSection() {
             direction: 'alternate',
             easing: 'easeInOutSine',
         });
-    
+
         // Anime.js for dir-alternate element with opacity change
         anime({
             targets: '.dir-alternate',
             translateX: 250,
-            opacity: [1,0.3], 
+            opacity: [1, 0.3], 
             direction: 'alternate',
             easing: 'easeInOutSine',
         });
     }, []); 
-    
+
     const openLogin = () => {
-        setShowChatbot(true);
+        const token = localStorage.getItem('userToken');
+        if (token) {
+            router.push('/dashboard'); 
+        } else {
+            setShowChatbot(true);
+        }
     };
 
     const closeChatbot = () => {
         setShowChatbot(false);
     };
+
+    const handleDashboardClick = () => {
+        const token = localStorage.getItem('userToken');
+        if (token) {
+            router.push('/dashboard'); 
+        } else {
+            openLogin(); 
+            alert("Your tocken expired login again!");
+        }
+    };
+
     return (
         <>
-            <div className=" items-center flex flex-col  justify-center w-full  topsection">
-                <div className='w-full items-center flex flex-col  justify-center inner-div '>
-                    <div className='h-fit flex  items-center justify-center flex-col text-div'>
+            <div className="items-center flex flex-col justify-center w-full topsection">
+                <div className='w-full items-center flex flex-col justify-center inner-div'>
+                    <div className='h-fit flex items-center justify-center flex-col text-div'>
                         <h1 className='text-center dir-normal'>TRADE FOREX WITH <strong className='automated'>AUTOMATED</strong> PRECISION</h1>
-                        <p className='text-m text-white opacity-80 text-center dir-reverse'>Harness the power of algorithmic trading. Let our system trade for you while you watch your portfolio grow, 24/7.</p>
+                        <p className='text-m text-white opacity-60 text-center dir-reverse'>Harness the power of algorithmic trading. Let our system trade for you while you watch your portfolio grow, 24/7.</p>
 
-                        <div className='w-fit h-fit flex  mt-20  top-links  dir-alternate'>
-                            <button className="links-in-top flex flex row  justify-items-center items-center inside-utton " onClick={openLogin}>
-                                Start Trading  &gt;
+                        <div className='w-fit h-fit flex mt-20 top-links dir-alternate'>
+                            <button className="links-in-top flex flex row justify-items-center items-center inside-utton" onClick={openLogin}>
+                                Login &gt;
                             </button>
 
-                            <a className="links-in-top flex flex row  justify-items-center items-center inside-utton2 " href='/#how-it-work'>
-                                How it Works  &gt;
+                            <a className="links-in-top flex flex row justify-items-center items-center inside-utton2" onClick={handleDashboardClick}>
+                                Your dashboard &gt;
                             </a>
                         </div>
-
                     </div>
-
                 </div>
 
                 {/* Conditional rendering for the chatbot overlay */}
                 {showChatbot && (
-                    <div className="overlay w-full h-full ">
+                    <div className="overlay w-full h-full">
                         <div className="modal w-full h-full">
                             <div className="close" onClick={closeChatbot}>&times;</div>
                             <Login />
                         </div>
                     </div>
                 )}
-
             </div>
-
         </>
     );
 }

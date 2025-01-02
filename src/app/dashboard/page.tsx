3af5cfb763e1e994037c1335b07c71a3ad7ec5ec
Daniel wame <wamedaniel9@gmail.com>
2025-01-02 11:58:09 +0200

    restructuring dashoard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -47,7 +47,7 @@ const Dashboard = () => {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     setSearchParams(params);
-  }, []); 
+  }, []);
 
   // Get the query parameters after the searchParams state is set
   const acct1 = searchParams?.get('acct1');
@@ -65,12 +65,12 @@ const Dashboard = () => {
       const token = localStorage.getItem('userToken');
       if (token) {
         authorizeUser(token);
-      }else {
+      } else {
         // router.replace('/');
         // alert("No tocken found please login first1");
       }
     }
-  }, [acct1, token1, cur1]); 
+  }, [acct1, token1, cur1]);
 
   const authorizeUser = async (token: string) => {
     console.log("Sending token:", token);
@@ -105,65 +105,109 @@ const Dashboard = () => {
   };
 
   return (
-    <div className='w-full min-h-screen dashboard flex justify-center'>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+    <div className='dashoard'>
+      <div id='nav-bar'>
+        <h1 className='logo'>FX <strong className='trading'>TRADING</strong></h1>
+      </div>
+      <div className='dashboard-conntent'>
+        <div className='small-header'></div>
+        <div className='accounts-info'>
+          <div className='names-div'>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
 
-      {(
-        // If authorization data is present, display dashboard
-        authorizeData ? (
-          <div className='w-full h-fit flex flex-col justify-center insidediv'>
-            <div className='w-full dashboard-imge'>
-              <Header />
-              <div className='w-ful flex justify-center items-center personalinfo-div'>
-                <div className='namesandemail'>
+            {(
+              // If authorization data is present, display dashboard
+              authorizeData ? (
+                <div>
+                  <h2>Personal Info.</h2>
                   <h1 className='fulname'>{authorizeData.authorize.authorize.fullname}</h1>
-                  <p className='text-m emailinheader opacity-90'>{authorizeData.authorize.authorize.email}</p>
+                  <p className='text-m emailinheader2 opacity-90'>{authorizeData.authorize.authorize.email}</p>
+
                 </div>
-                <div className='w-fit h-fit flex top-links'>
-                  <button className="links-in-top flex flex row justify-items-center items-center inside-utton">
-                    Start Trading &gt;
-                  </button>
-                  <a className="links-in-top flex flex row justify-items-center items-center inside-utton2" href='/#how-it-work'>
-                    Current Analysis &gt;
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className='mt-4 balancediv'>
-              <h2>My Balance</h2>
-              <h1>{authorizeData.authorize.authorize.balance} {authorizeData.authorize.authorize.currency}</h1>
-              <div className='w-ful flex justify-center mt-4 pt-2 small-info'>
-                <p className='text-m text-black opacity-70'>
-                  <strong>Account Type:</strong> {authorizeData.authorize.authorize.account_list[0].account_type}
-                </p>
-                <p className='text-m text-black opacity-70'><strong>Login ID:</strong> {authorizeData.authorize.authorize.loginid}</p>
-                <p className='text-m text-black opacity-70'><strong>Country:</strong> {authorizeData.authorize.authorize.country}</p>
-                <p className='text-m text-black opacity-70'><strong>Local Currencies:</strong> {Object.keys(authorizeData.authorize.authorize.local_currencies).join(', ')}</p>
-                <p className='text-m text-black opacity-70'><strong>User ID:</strong> {authorizeData.authorize.authorize.user_id}</p>
-                <p className='text-m text-red-500 opacity-70'><strong className='text-red-500'>Broker:</strong> {authorizeData.authorize.authorize.landing_company_fullname}</p>
-                <p className='text-m text-black opacity-70'>{authorizeData.authorize.authorize.account_list[0].account_category} account</p>
-              </div>
-            </div>
-            <div className='mt-8 balancediv'>
-              <h2>Current Analysis</h2>
-              <SupersetDashboard/>
-            </div>
-
-            <div className='mt-8 balancediv'>
-              <h2>Trade History</h2>
-            </div>
-
-            <div className='mt-8 balancediv'>
-              <h2>Profit and Loss</h2>
-            </div>
+              ) : (
+                <p className='text-center'>Loading authorization...</p>
+              )
+            )}
           </div>
-        ) : (
-          <p className='mt-40'>Loading authorization...</p>
-        )
-      )}
+          <div className='names-div'>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+
+            {(
+              // If authorization data is present, display dashboard
+              authorizeData ? (
+                <div>
+                  <h2>My Balance</h2>
+                  <h1>{authorizeData.authorize.authorize.balance} {authorizeData.authorize.authorize.currency}</h1>
+
+                </div>
+              ) : (
+                <p className='text-center'>Loading authorization...</p>
+              )
+            )}
+          </div>
+        </div>
+        <div className='superset-chatrs-div'></div>
+      </div>
+
     </div>
-  );
+  )
 };
 
 export default Dashboard;
+
+// {error && <p style={{ color: 'red' }}>{error}</p>}
+
+// {(
+//   // If authorization data is present, display dashboard
+//   authorizeData ? (
+//     <div className='w-full h-fit flex flex-col justify-center insidediv'>
+//       <div className='w-full dashboard-imge'>
+//         <Header />
+//         <div className='w-ful flex justify-center items-center personalinfo-div'>
+//           <div className='namesandemail'>
+//             <h1 className='fulname'>{authorizeData.authorize.authorize.fullname}</h1>
+//             <p className='text-m emailinheader opacity-90'>{authorizeData.authorize.authorize.email}</p>
+//           </div>
+//           <div className='w-fit h-fit flex top-links'>
+//             <button className="links-in-top flex flex row justify-items-center items-center inside-utton">
+//               Start Trading &gt;
+//             </button>
+//             <a className="links-in-top flex flex row justify-items-center items-center inside-utton2" href='/#how-it-work'>
+//               Current Analysis &gt;
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className='mt-4 balancediv'>
+//         <h2>My Balance</h2>
+//         <h1>{authorizeData.authorize.authorize.balance} {authorizeData.authorize.authorize.currency}</h1>
+//         <div className='w-ful flex justify-center mt-4 pt-2 small-info'>
+//           <p className='text-m text-black opacity-70'>
+//             <strong>Account Type:</strong> {authorizeData.authorize.authorize.account_list[0].account_type}
+//           </p>
+//           <p className='text-m text-black opacity-70'><strong>Login ID:</strong> {authorizeData.authorize.authorize.loginid}</p>
+//           <p className='text-m text-black opacity-70'><strong>Country:</strong> {authorizeData.authorize.authorize.country}</p>
+//           <p className='text-m text-black opacity-70'><strong>Local Currencies:</strong> {Object.keys(authorizeData.authorize.authorize.local_currencies).join(', ')}</p>
+//           <p className='text-m text-black opacity-70'><strong>User ID:</strong> {authorizeData.authorize.authorize.user_id}</p>
+//           <p className='text-m text-red-500 opacity-70'><strong className='text-red-500'>Broker:</strong> {authorizeData.authorize.authorize.landing_company_fullname}</p>
+//           <p className='text-m text-black opacity-70'>{authorizeData.authorize.authorize.account_list[0].account_category} account</p>
+//         </div>
+//       </div>
+//       <div className='mt-8 balancediv'>
+//         <h2>Current Analysis</h2>
+//         <SupersetDashboard />
+//       </div>
+
+//       <div className='mt-8 balancediv'>
+//         <h2>Trade History</h2>
+//       </div>
+
+//       <div className='mt-8 balancediv'>
+//         <h2>Profit and Loss</h2>
+//       </div>
+//     </div>
+//   ) : (
+//     <p className='mt-40'>Loading authorization...</p>
+//   )
+// )}

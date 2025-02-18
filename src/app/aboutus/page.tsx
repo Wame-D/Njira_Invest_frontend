723bade92502d04b1c360cee1eb9141e68580a72
bsc-com-05-20 <127213273+bsc-com-05-20@users.noreply.github.com>
2025-02-18 07:01:21 +0200

    Updated team members images
    Updated team members images

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -4,10 +4,10 @@ import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram } from "react-icons/f
 
 export default function About(){
   const members = [
-    { name: "Peter Kayira", position: "Front-End Developer", image: "/placeholder.jpg" },
-    { name: "Richard Mlambuzi", position: "Trading Strategist", image: "/placeholder.jpg" },
-    { name: "Daniel Wame", position: "Market Analyst", image: "/placeholder.jpg" },
-    { name: "Melvin Kalidozo", position: "Trade Execution Specialist", image: "/placeholder.jpg" },
+    { name: "Peter Kayira", position: "Front-End Developer", image: "https://scontent.fblz2-1.fna.fbcdn.net/v/t39.30808-6/460370590_17985617756721539_7305534897771603808_n.jpg?stp=dst-jpg_tt6&_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFT5EXillj2O0PYInLqt3wFY4MeacJirB1jgx5pwmKsHTOeRcnv4rNJdkNsfxf0Lk9Fpo0OywwkOin36FBAUHeY&_nc_ohc=Z7Lz0BS4mlcQ7kNvgGvvlvO&_nc_zt=23&_nc_ht=scontent.fblz2-1.fna&_nc_gid=AKo1IPzutvFn6RYLbDXQgJK&oh=00_AYBqhzigo96tCFY48UmHixDFk3qZYHC5TRPsRYFLsJZOrA&oe=67B9F09B" },
+    { name: "Richard Mlambuzi", position: "Trading Strategist", image: "https://avatars.githubusercontent.com/u/95221011?v=4" },
+    { name: "Daniel Wame", position: "Market Analyst", image: "https://avatars.githubusercontent.com/u/119085857?v=4" },
+    { name: "Melvin Kalidozo", position: "Trade Execution Specialist", image: "https://scontent.fblz2-1.fna.fbcdn.net/v/t39.30808-6/462695211_2592965444207395_5286495986513675172_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGjFMce3hsLDMNFeCpNI7l3LmthFglfzY0ua2EWCV_NjZseAGNSeVU3RqVq7rbAcqf-qmBtFEgkfb8UUXbeb3PO&_nc_ohc=w9ixIDtdlyMQ7kNvgFsO4tn&_nc_zt=23&_nc_ht=scontent.fblz2-1.fna&_nc_gid=A9aamcndbYpz_XpxMtP2-en&oh=00_AYA_h9g_0rzjkf5FiMf7O-1Pd5qUVv6aGiWPCgsAL1qSMg&oe=67B9E18A" },
   ];
   return(
     <div>

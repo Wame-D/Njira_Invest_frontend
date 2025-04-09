8325318d75f9a5537681e366a6d9bb6c3549ed54
bsc-com-05-20 <127213273+bsc-com-05-20@users.noreply.github.com>
2025-04-10 01:07:05 +0200

    Replicated start trading with us button from top section to begin trading section
    Replicated start trading with us button from top section to begin trading section

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -44,17 +44,17 @@ const Contact = () => {
 <input
   type="email"
   placeholder="Input your email"
-  className="p-2 w-full border border-sky-500 rounded-md"
+  className="p-2 w-full border border-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-500 rounded-md"
 />
             </div>
             <input
   type="text"
   placeholder="Subject"
-  className="p-2 w-full border border-sky-500 rounded-md"
+  className="p-2 w-full border border-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-500 rounded-md"
 />
 <textarea
   placeholder="Submit your message request"
-  className="p-2 w-full border border-sky-500 rounded-md h-24"
+  className="p-2 w-full border border-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-500 rounded-md h-24"
 />
             <button className="bg-darkBlue text-white px-4 py-2 rounded-md">Send message</button>
           </form>

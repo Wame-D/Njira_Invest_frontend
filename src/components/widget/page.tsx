81f98b26a12c2b59bc8135adcc4a47c4bb686535
bsc-com-05-20 <127213273+bsc-com-05-20@users.noreply.github.com>
2025-04-22 21:48:46 +0200

    Removed full market preview on EURO/USD symbol
    Removed full market preview on EURO/USD symbol

--- src/components/widget/page.tsx
+++ src/components/widget/page.tsx
@@ -33,9 +33,7 @@ const TradingViewWidget = () => {
     <div className="tradingview-widget-container mt-16 w-full max-w-6xl">
       <div className="tradingview-widget-container__widget"></div>
       <div className="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
-          <span className="blue-text">Track all markets on TradingView</span>
-        </a>
+        
       </div>
     </div>
   );

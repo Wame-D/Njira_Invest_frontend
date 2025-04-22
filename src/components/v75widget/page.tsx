59cb5c8851bac845b28db6d84f45a665573e6444
bsc-com-05-20 <127213273+bsc-com-05-20@users.noreply.github.com>
2025-04-22 22:51:13 +0200

    Placed all symbols widgets on home page
    Placed all symbols widgets on home page

--- src/components/v75widget/page.tsx
+++ src/components/v75widget/page.tsx
@@ -32,9 +32,7 @@ const V75Widget = () => {
     <div className="tradingview-widget-container">
       <div className="tradingview-widget-container__widget"></div>
       <div className="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
-          <span className="blue-text">Track all markets on TradingView</span>
-        </a>
+        
       </div>
     </div>
   );
